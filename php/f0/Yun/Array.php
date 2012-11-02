53505be48cb123c50e670e2a1136bc8b2095484a
walu <walu@localhost.(none)>
2012-11-02 08:33:48 +0800

    Yun_Array::getByPath is finished.

--- php/f0/Yun/Array.php
+++ php/f0/Yun/Array.php
@@ -22,12 +22,25 @@ class Yun_Array {
 		return false!==array_key_exists($key, $arr) ? $arr[$key] : $default_value;
 	}
 	
+	/**
+	 * 根据一个带格式的key，来获取数组中的某个值
+	 * 
+	 * @param array $arr
+	 * @param string|number $path_key
+	 * @param mixed $default_value
+	 * @param string $path_separator 必须为string，否则请用Yun_Array::get
+	 * @return mixed
+	 */
 	public static function getByPath(array $arr, $path_key, $default_value=null, $path_separator='.') {
-		
-	}
-	
-	public static function rekey() {
-		
+		$path_key_arr = explode($path_separator, $path_key);
+		$retval = $arr;
+		foreach ($path_key_arr as $current_key) {
+			$retval = self::get($retval, $current_key);
+			if (!is_array($retval)) {
+				break;
+			}
+		}
+		return false === next($path_key_arr) ? $retval : $default_value;
 	}
 	
 	public static function rekeyByPath() {
