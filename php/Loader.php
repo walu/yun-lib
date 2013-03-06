1069fff6dd92befc93c1ddebec1596521b3230ea
walu <imcnan@gmail.com>
2013-03-06 10:27:49 +0800

    fix Loader bugs.

--- php/Loader.php
+++ php/Loader.php
@@ -1,14 +1,15 @@
 <?php
 $yun_lib_path = dirname(__FILE__).'/';
 function Yun_Loader($class_name) {
-	$path = str_replace('_', '/', $class_name) . '.php';
-	$path_arr = array('f0', 'f1', 'f2');
-	foreach ($path_arr as $floor) {
-		$tmppath = "{$floor}/{$path}";
-		if (file_exists($tmppath)) {
-			require_once($tmppath);
-			return;
-		}
-	}
+    global $yun_lib_path;
+    $path = str_replace('_', '/', $class_name) . '.php';
+    $path_arr = array('f0', 'f1', 'f2');
+    foreach ($path_arr as $floor) {
+        $tmppath = "{$yun_lib_path}/{$floor}/{$path}";
+        if (file_exists($tmppath)) {
+            require_once($tmppath);
+            return;
+        }
+    }
 }
 spl_autoload_register("Yun_Loader");
