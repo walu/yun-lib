8eaa949f1195f29d9affe981b8b5830263f50da7
walu <imcnan@gmail.com>
2013-05-08 13:52:33 +0800

    add Yun_String::randomString

--- Tests/f0/Yun/StringTest.php
+++ Tests/f0/Yun/StringTest.php
@@ -17,4 +17,15 @@ class Yun_StringTest extends PHPUnit_Framework_TestCase {
 		$re = Yun_String::safeExplode(',', $string);
 		$this->assertTrue(in_array('2', $re) && count($re)==2);
 	}
+
+    public function test_randomString() {
+        $re = Yun_String::randomString(100);
+        $this->assertEquals(100, strlen($re));
+
+        $re = Yun_String::randomString(5, '0');
+        $this->assertEquals('00000', $re);
+
+        $re = Yun_String::randomString(5, '');
+        $this->assertEmpty($re);
+    }
 }
